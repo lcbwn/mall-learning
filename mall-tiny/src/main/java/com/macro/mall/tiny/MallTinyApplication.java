7f022a0ac33f76969b2f39ef8bac5c8810f9a6b1
robin <lcbwn@163.com>
2020-02-13 17:42:26 +0800

    robin1

--- mall-tiny/src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ mall-tiny/src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -2,7 +2,7 @@ package com.macro.mall.tiny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//robin
+//robin1
 @SpringBootApplication
 public class MallTinyApplication {
 
